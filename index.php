418850ba7881b846a5d5f861f283ff9fa6df4d3f
Ilya <illyaetoilysha@gmail.com>
2022-03-18 16:55:21 +0200

    Enter

--- index.php
+++ index.php
@@ -11,10 +11,11 @@ const TOKEN = '5120451737:AAFEQynxE0tBu1LGrB5lId0VKptHnRV4hXI';
 $telegram = new TelegramBot(TOKEN);
 $update = $telegram->getWebhookUpdates();
 
+file_put_contents(__DIR__ . '/logs.txt', print_r($update, 1), FILE_APPEND);
+
 $users_id = $update['message']['chat']['id'];
-$users_id2 = $update['callback_query']['message']['chat']['id'];
+$users_id_callback_query = $update['callback_query']['message']['chat']['id'];
 $callback_id = $update['callback_query']['id'];
-$callback_data = $update['callback_query']['data'];
 $users_name = $update['message']['from']['first_name'];
 $users_text = $update['message']['text'];
 $inline_type = $update['callback_query']['data'];
