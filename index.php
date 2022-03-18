2bece7c4532e9b06b409049d916c10215c1f0461
Ilya <illyaetoilysha@gmail.com>
2022-03-18 16:52:09 +0200

    Enter

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ require_once __DIR__ . '/TelegramBot.php';
 use \Dejurin\GoogleTranslateForFree;
 
 const TOKEN = '5120451737:AAFEQynxE0tBu1LGrB5lId0VKptHnRV4hXI';
+
+$telegram = new TelegramBot(TOKEN);
+$update = $telegram->getWebhookUpdates();
+
+$users_id = $update['message']['chat']['id'];
+$users_id2 = $update['callback_query']['message']['chat']['id'];
+$callback_id = $update['callback_query']['id'];
+$callback_data = $update['callback_query']['data'];
+$users_name = $update['message']['from']['first_name'];
+$users_text = $update['message']['text'];
+$inline_type = $update['callback_query']['data'];
