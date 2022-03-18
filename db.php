55697b4fe4a1757229fe4665eddf6593a2954fe7
Ilya <illyaetoilysha@gmail.com>
2022-03-18 18:49:09 +0200

    E

--- db.php
+++ db.php
@@ -10,7 +10,14 @@ $db = [
 $dsn = "mysql:host={$db['host']};dbname={$db['db']};charset=utf8";
 $opt = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 $pdo = new PDO($dsn, $db['user'], $db['pass'], $opt);
+
+function get_chat_id($chat_id) {
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT * FROM chat WHERE users_id = ?");
+    $stmt->execute([$chat_id]);
+    return $stmt->fetch();
+}
