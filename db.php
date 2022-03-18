16b6a92d707f45698cb47bf2d352cde9325f8c08
Ilya <illyaetoilysha@gmail.com>
2022-03-18 18:29:08 +0200

    Enter

--- db.php
+++ db.php
@@ -6,3 +6,11 @@ $db = [
     'pass' => 'aj71j1ppnwzm0m44sf',
     'db' => 'i98795_db'
 ];
+
+$dsn = "mysql:host={$db['host']};dbname={$db['db']};charset=utf8";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$pdo = new PDO($dsn, $db['user'], $db['pass'], $opt);
